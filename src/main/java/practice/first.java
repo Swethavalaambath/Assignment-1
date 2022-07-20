2a06e9c895b0647143a0cc47025e0790a6656356
swetha <swetha.valambath@gmail.com>
2022-07-20 09:53:46 -0500

    git assignment 1

---
+++ src/main/java/practice/first.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class first {
+
+	public static void main(String[] args) {
+		
+		System.out.println("learning maven and git");// TODO Auto-generated method stub
+
+	}
+
+}
